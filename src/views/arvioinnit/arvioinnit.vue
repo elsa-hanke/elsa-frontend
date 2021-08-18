53637a05896577aa31f9d98df17b1b0e8320ca78
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-08-18 09:30:14 +0300

    UOELSA-570: Vastuuhenkilön arviointipyynnöt
    - Lisää tarkistus onko kirjautunut käyttäjä vastuuhenkilö, arvioinnit näkymää ladattaessa

--- src/views/arvioinnit/arvioinnit.vue
+++ src/views/arvioinnit/arvioinnit.vue
@@ -1,5 +1,6 @@
 <template>
   <ArvioinnitErikoistuva v-if="$isErikoistuva()" />
+  <ArvioinnitVastuuhenkilo v-else-if="$isVastuuhenkilo()" />
   <ArvioinnitKouluttaja v-else />
 </template>
 
@@ -7,11 +8,13 @@
   import { Component, Vue } from 'vue-property-decorator'
   import ArvioinnitErikoistuva from '@/views/arvioinnit/arvioinnit-erikoistuva.vue'
   import ArvioinnitKouluttaja from '@/views/arvioinnit/arvioinnit-kouluttaja.vue'
+  import ArvioinnitVastuuhenkilo from '@/views/arvioinnit/arvioinnit-vastuuhenkilo.vue'
 
   @Component({
     components: {
       ArvioinnitErikoistuva,
-      ArvioinnitKouluttaja
+      ArvioinnitKouluttaja,
+      ArvioinnitVastuuhenkilo
     }
   })
   export default class Arvioinnit extends Vue {}
