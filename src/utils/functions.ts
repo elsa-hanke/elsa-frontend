d2fea55b0179c81f5b10efd75181e44f9d76035c
Sauli Anto <sauli.anto@gofore.com>
2021-09-06 16:30:39 +0300

    UOELSA-502: Toteutettu profiilinäkymät

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { ELSA_ROLE } from './roles'
+
 export const clamp = (num: number, clamp: number, higher: number) =>
   higher ? Math.min(Math.max(num, clamp), higher) : Math.min(num, clamp)
 
@@ -6,4 +8,18 @@ export function checkCurrentRouteAndRedirect(router: any, path: any) {
     currentRoute: { path: curPath }
   } = router
   if (curPath !== path) router.push({ path })
+}
+
+export function getTitleFromAuthorities(authorities: string[]) {
+  if (authorities.includes(ELSA_ROLE.ErikoistuvaLaakari)) {
+    return 'erikoistuva-laakari'
+  } else if (authorities.includes(ELSA_ROLE.Kouluttaja)) {
+    return 'kouluttaja'
+  } else if (authorities.includes(ELSA_ROLE.Lahikouluttaja)) {
+    return 'lahikouluttaja'
+  } else if (authorities.includes(ELSA_ROLE.Vastuuhenkilo)) {
+    return 'vastuuhenkilo'
+  }
+
+  return undefined
 }
---
+++ src/views/profiili/profiili.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="profiili">
+    <b-breadcrumb :items="items" class="mb-0" />
+    <b-container fluid>
+      <b-row lg>
+        <b-col>
+          <h1>{{ $t('oma-profiili') }}</h1>
+          <p>{{ $t('oma-profiili-kuvaus') }}</p>
+          <b-tabs content-class="mt-3" :no-fade="true">
+            <b-tab :title="$t('omat-tiedot')" active>
+              <omat-tiedot :editing="editing" @change="changeEditing" />
+            </b-tab>
+            <b-tab :title="$t('katseluoikeudet')">
+              <!-- TODO: katseluoikeudet komponentti -->
+            </b-tab>
+          </b-tabs>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Mixins } from 'vue-property-decorator'
+  import ConfirmRouteExit from '@/mixins/confirm-route-exit'
+  import OmatTiedot from './omat-tiedot.vue'
+
+  @Component({
+    components: {
+      OmatTiedot
+    }
+  })
+  export default class Profiili extends Mixins(ConfirmRouteExit) {
+    items = [
+      {
+        text: this.$t('etusivu'),
+        to: { name: 'etusivu' }
+      },
+      {
+        text: this.$t('oma-profiili'),
+        active: true
+      }
+    ]
+    editing = false
+    skipRouteExitConfirm = true
+
+    changeEditing(event: boolean) {
+      this.skipRouteExitConfirm = !event
+      this.editing = event
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .profiili {
+    max-width: 768px;
+  }
+</style>
