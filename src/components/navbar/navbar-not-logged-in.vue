8b4e10f17368bc75d8a96ebfed6c73f834df09c6
Heikki Ala-Nikkola <heikki.ala-nikkola@gofore.com>
2022-01-27 12:11:57 +0200

    UOELSA-892: Lisää logoon route etusivulle, päivitetyt logotyylit

--- src/components/navbar/navbar-not-logged-in.vue
+++ src/components/navbar/navbar-not-logged-in.vue
@@ -1,14 +1,16 @@
 <template>
   <b-navbar id="navbar-top" type="dark" variant="primary" sticky class="px-0 py-0">
     <b-container>
-      <b-navbar-brand class="col text-nowrap user-select-none">
-        <div class="brand-logo d-inline-block font-weight-bold text-uppercase">
-          {{ $t('elsa') }}
-        </div>
-        <div class="brand-text d-inline-block text-size-sm align-top pt-2">
-          -{{ $t('palvelu') | lowercase }}
-        </div>
-      </b-navbar-brand>
+      <router-link to="/etusivu">
+        <b-navbar-brand class="col text-nowrap user-select-none">
+          <div class="brand-logo d-inline-block font-weight-bold text-uppercase">
+            {{ $t('elsa') }}
+          </div>
+          <div class="brand-text d-inline-block text-size-sm align-top pt-2">
+            -{{ $t('palvelu') | lowercase }}
+          </div>
+        </b-navbar-brand>
+      </router-link>
       <!-- Piilotetaan pilotista -->
       <!-- <b-navbar-nav class="ml-auto pr-3 font-weight-500">
         <b-nav-item-dropdown :text="$t(currentLocale)" class="align-self-center px-3" right>
@@ -49,7 +51,24 @@
 </script>
 
 <style lang="scss" scoped>
+  @import '~@/styles/variables';
+  @import '~bootstrap/scss/mixins/breakpoints';
+
+  .brand-logo {
+    font-size: 2.1875rem;
+    line-height: 2.375rem;
+    @include media-breakpoint-down(sm) {
+      font-size: 1.875rem;
+    }
+  }
+
   .brand-text {
-    font-size: 0.725rem;
+    font-size: 0.9375rem;
+    position: relative;
+    top: -10px;
+    @include media-breakpoint-down(sm) {
+      font-size: 0.8125rem;
+      top: -7px;
+    }
   }
 </style>
