03a4c9ce5f03634fd08ad85853928c09bfe279f1
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-05-02 12:18:44 +0300

    UOELSA-826: Poista käyttämättömät roolit

--- src/utils/roles.ts
+++ src/utils/roles.ts
@@ -1,13 +1,8 @@
 /* eslint-disable no-unused-vars */
 export enum ELSA_ROLE {
   ErikoistuvaLaakari = 'ROLE_ERIKOISTUVA_LAAKARI',
-  ErikoistuvaLaakariImpersonated = 'ROLE_ERIKOISTUVA_LAAKARI_IMPERSONATED',
   Kouluttaja = 'ROLE_KOULUTTAJA',
-  Koulutusdekaani = 'ROLE_KOULUTUSDEKAANI',
-  Lahikouluttaja = 'ROLE_LAHIKOULUTTAJA',
   OpintohallinnonVirkailija = 'ROLE_OPINTOHALLINNON_VIRKAILIJA',
-  Paakayttaja = 'ROLE_PAAKAYTTAJA',
-  PalvelujarjestelmanVirkailija = 'ROLE_PALVELUJARJESTELMAN_VIRKAILIJA',
   TekninenPaakayttaja = 'ROLE_TEKNINEN_PAAKAYTTAJA',
   Vastuuhenkilo = 'ROLE_VASTUUHENKILO'
 }
