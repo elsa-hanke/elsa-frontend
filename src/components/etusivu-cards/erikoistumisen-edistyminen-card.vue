38639e37e1821684421c630b9ddb3baa750e2f4f
tviljaka <81751595+tviljaka@users.noreply.github.com>
2022-08-10 16:00:43 +0300

    UOELSA-877: Lisää päivittäiset merkinnät etusivulle

--- src/components/etusivu-cards/erikoistumisen-edistyminen-card.vue
+++ src/components/etusivu-cards/erikoistumisen-edistyminen-card.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-card-skeleton :header="$t('erikoistumisen-edistyminen')" :loading="initializing">
+  <b-card-skeleton :header="$t('erikoistumisen-edistyminen')" :loading="initializing" class="mb-5">
     <b-container v-if="!initializing" fluid class="p-0">
       <b-row>
         <b-col xl="8">
