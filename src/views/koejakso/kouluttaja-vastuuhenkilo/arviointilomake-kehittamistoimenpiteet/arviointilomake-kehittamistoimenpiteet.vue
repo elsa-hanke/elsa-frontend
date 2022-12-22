bd48aef8c9308aab3037a2439e6c58b33c002baf
Heikki Ala-Nikkola <heikki.ala-nikkola@gofore.com>
2022-12-22 15:10:06 +0200

    UOELSA-1219: fix: palautusnapin disablointi jos lähiesimies ja kouluttaja sama (#330)

--- src/views/koejakso/kouluttaja-vastuuhenkilo/arviointilomake-kehittamistoimenpiteet/arviointilomake-kehittamistoimenpiteet.vue
+++ src/views/koejakso/kouluttaja-vastuuhenkilo/arviointilomake-kehittamistoimenpiteet/arviointilomake-kehittamistoimenpiteet.vue
@@ -153,7 +153,10 @@
                 {{ $t('peruuta') }}
               </elsa-button>
               <elsa-button
-                v-if="isCurrentUserLahiesimies"
+                v-if="
+                  isCurrentUserLahiesimies &&
+                  kehittamistoimenpiteet.lahiesimies.id != kehittamistoimenpiteet.lahikouluttaja.id
+                "
                 class="my-2 mr-3 d-block d-md-inline-block d-lg-block d-xl-inline-block"
                 style="min-width: 14rem"
                 variant="outline-primary"
