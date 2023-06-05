c641c1eeebece182769c5dce1632efefcdbef651
Arttu Hänninen <arttu.hanninen@gofore.com>
2023-06-05 14:15:50 +0300

    UOELSA-1304: Korjaus kehittämistoimenpiteiden näyttämiseen

--- src/views/koejakso/kouluttaja-vastuuhenkilo/arviointilomake-loppukeskustelu/arviointilomake-loppukeskustelu.vue
+++ src/views/koejakso/kouluttaja-vastuuhenkilo/arviointilomake-loppukeskustelu/arviointilomake-loppukeskustelu.vue
@@ -131,7 +131,7 @@
           <hr />
         </div>
 
-        <div v-if="loppukeskustelu.kehittamistoimenpiteet">
+        <div v-if="loppukeskustelu.kehittamistoimenpiteetRiittavat != null">
           <h3 class="mb-3">{{ $t('kehittamistoimenpiteiden-arviointi') }}</h3>
           <p v-if="loppukeskustelu.kehittamistoimenpiteetRiittavat">
             {{ $t('kehittamistoimenpiteet-riittavat') }}
