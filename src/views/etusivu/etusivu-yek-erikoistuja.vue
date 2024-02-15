35a0eb79f4db8d6183927723d4578ba3e825e945
Martin Kankaanranta <martin.kankaanranta@gofore.com>
2024-02-15 16:04:37 +0200

    yek näkymien kehitystä

--- src/views/etusivu/etusivu-yek-erikoistuja.vue
+++ src/views/etusivu/etusivu-yek-erikoistuja.vue
@@ -3,7 +3,7 @@
     <b-row class="mt-3">
       <b-col xxl="9">
         <avoimet-asiat-card />
-        <erikoistumisen-edistyminen-card />
+        <yek-koulutuksen-edistyminen-card />
       </b-col>
       <b-col xxl="3">
         <b-row>
@@ -21,15 +21,15 @@
 
   import BCardSkeleton from '@/components/card/card.vue'
   import AvoimetAsiatCard from '@/components/etusivu-cards/avoimet-asiat-card.vue'
-  import ErikoistumisenEdistyminenCard from '@/components/etusivu-cards/erikoistumisen-edistyminen-card.vue'
   import HenkilotiedotCard from '@/components/etusivu-cards/henkilotiedot-card.vue'
+  import YekKoulutuksenEdistyminenCard from '@/components/etusivu-cards/yek-koulutuksen-edistyminen-card.vue'
 
   @Component({
     components: {
       BCardSkeleton,
       AvoimetAsiatCard,
-      ErikoistumisenEdistyminenCard,
-      HenkilotiedotCard
+      HenkilotiedotCard,
+      YekKoulutuksenEdistyminenCard
     }
   })
   export default class EtusivuErikoistuja extends Vue {}
