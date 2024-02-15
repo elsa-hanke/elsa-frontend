b13dd925538bb200dec8ec2806461a16d7a661cb
Martin Kankaanranta <martin.kankaanranta@gofore.com>
2024-02-15 09:56:55 +0200

    Lisätty yek erikoisala tarkistus erikoistuvan roolin tarkistukseen

--- src/plugins/roles.ts
+++ src/plugins/roles.ts
@@ -2,11 +2,28 @@
 import Vue from 'vue'
 
 import store from '@/store'
+import { Opintooikeus } from '@/types'
+import { ERIKOISALA_YEK_ID } from '@/utils/constants'
 import { ELSA_ROLE } from '@/utils/roles'
 
 export class RolesPlugin {
   public install(vue: typeof Vue) {
     vue.prototype.$isErikoistuva = (): boolean => {
+      const data = store.getters['auth/account']
+      const { opintooikeudet, opintooikeusKaytossaId } = data.erikoistuvaLaakari
+      const opintooikeus = opintooikeudet.filter(
+        (oo: Opintooikeus) => oo.id === opintooikeusKaytossaId
+      )
+      if (opintooikeus.length === 0) {
+        return store.getters['auth/account'].activeAuthority === ELSA_ROLE.ErikoistuvaLaakari
+      }
+      return (
+        store.getters['auth/account'].activeAuthority === ELSA_ROLE.ErikoistuvaLaakari &&
+        opintooikeus[0].erikoisalaId !== ERIKOISALA_YEK_ID
+      )
+    }
+
+    vue.prototype.$isYekErikoistuva = (): boolean => {
       return store.getters['auth/account'].activeAuthority === ELSA_ROLE.ErikoistuvaLaakari
     }
 
