c4fc1b994fd424ba36eff182f35ed4cc85977e6a
Juho Turkki <juho.turkki@gofore.com>
2025-02-10 08:23:35 +0200

    ELSA-818: Vastuuhenkilön valmistumispyynnön avoimet tehtävät (#516)
    * ELSA-818: Piilotetaan nappula, jos kyseisen käyttäjän ei ole vielä aika toimia
    
    * ELSA-818: Piilotetaan nappula, jos kyseisen käyttäjän ei ole vielä aika toimia ja lisätty odottaa virkailijan tarkistusta avoimen tilan sijaan

--- src/components/etusivu-cards/yek-valmistumispyynnot-card.vue
+++ src/components/etusivu-cards/yek-valmistumispyynnot-card.vue
@@ -132,6 +132,8 @@
 
     taskStatus(status: string) {
       switch (status) {
+        case ValmistumispyynnonTila.ODOTTAA_VASTUUHENKILON_TARKASTUSTA:
+          return this.$t('valmistumispyynnon-tila.avoimet-asiat-odottaa-osaamisen-arviointia')
         case ValmistumispyynnonTila.ODOTTAA_VASTUUHENKILON_HYVAKSYNTAA:
           return this.$t('valmistumispyynnon-tila.avoimet-asiat-odottaa-hyvaksyntaa')
         case ValmistumispyynnonTila.ODOTTAA_ALLEKIRJOITUKSIA:
@@ -143,7 +145,16 @@
       }
     }
     showButton(tila: ValmistumispyynnonTila) {
-      return tila === ValmistumispyynnonTila.ODOTTAA_ALLEKIRJOITUKSIA ? false : true
+      if (this.$isVirkailija())
+        return (
+          tila === ValmistumispyynnonTila.ODOTTAA_VIRKAILIJAN_TARKASTUSTA ||
+          tila === ValmistumispyynnonTila.VIRKAILIJAN_TARKASTUS_KESKEN
+        )
+      else
+        return (
+          tila === ValmistumispyynnonTila.ODOTTAA_VASTUUHENKILON_TARKASTUSTA ||
+          tila === ValmistumispyynnonTila.ODOTTAA_VASTUUHENKILON_HYVAKSYNTAA
+        )
     }
   }
 </script>
