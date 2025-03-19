0ada8be0187e6a4a0a7e2b1cbf5092e00502a3b8
Juho Turkki <juho.turkki@gofore.com>
2025-03-19 11:14:57 +0200

    ELSA-1604: Alusta arviointityökalujen esittelyyn

---
+++ src/views/arviointityokalut/esittely.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="esittely"></div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator'
+
+  @Component({})
+  export default class ArviointityokalutEsittely extends Vue {}
+</script>
+
+<style scoped lang="scss">
+  @import '~@/styles/variables';
+  @import '~bootstrap/scss/mixins/breakpoints';
+</style>
