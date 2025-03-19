c509da8207eb119bff14df591f020310524efe26
Juho Turkki <juho.turkki@gofore.com>
2025-03-19 11:42:59 +0200

    ELSA-1604: Esittelyn header

--- src/views/arviointityokalut/esittely.vue
+++ src/views/arviointityokalut/esittely.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="esittely"></div>
+  <div class="esittely">
+    <b-breadcrumb :items="items" class="mb-0" />
+    <b-container fluid>
+      <b-row lg>
+        <b-col>
+          <h1>{{ $t('arviointityokalut') }}</h1>
+          <p>{{ $t('arviointityokalut-esittely') }}</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script lang="ts">
   import { Vue, Component } from 'vue-property-decorator'
 
   @Component({})
-  export default class ArviointityokalutEsittely extends Vue {}
+  export default class ArviointityokalutEsittely extends Vue {
+    items = [
+      {
+        text: this.$t('etusivu'),
+        to: { name: 'etusivu' }
+      },
+      {
+        text: this.$t('arviointityokalut'),
+        active: true
+      }
+    ]
+  }
 </script>
 
 <style scoped lang="scss">
